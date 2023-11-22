aa78f8a421499a6732cc853347ae1a1d1de88400
Bhavnaarya <bhavnaarya47@gmail.com>
2023-11-22 11:42:07 +0530

    add new files

--- NumberGuess.java
+++ NumberGuess.java
@@ -1,5 +1,24 @@
+import java.util.Scanner;
+import java.lang.Math;
+
 public class NumberGuess{
     public static void main(String args[]){
-        System.out.println("hello");
+        System.out.println("Number Guessing game");
+        Scanner sc=new Scanner(System.in);
+        int num=(int)(Math.random()*100);
+        System.out.println(num);
+        int i=1;
+        while(i<6){
+        System.out.println("Enter any number b/w 0 to 100");
+        int a=sc.nextInt();
+        
+            if(a==num){
+                System.out.println("Correct answer");
+            }else{
+                System.out.println("Wrong answer!\nPlease try again");
+            }
+            i++;
+            }
+        }
+
     }
-}
